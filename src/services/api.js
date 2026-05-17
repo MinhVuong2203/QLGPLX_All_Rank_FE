bf6b10d660ffeeae45436af49b56a0fcbf827822
MinhVuong2203 <vuonghihihihi@gmail.com>
2026-05-17 09:23:55 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,8 @@
 import axios from 'axios'
 
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL
-// })
-
 const api = axios.create({
-  baseURL: 'https://qlgplxallrank-production.up.railway.app'})
+  baseURL: import.meta.env.VITE_API_BASE_URL
+})
 
 api.interceptors.request.use(
   (config) => {
